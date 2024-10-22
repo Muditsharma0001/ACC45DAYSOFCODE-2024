77856848dcde587b66d263bf98b02becd8cf6f58
Muditsharma0001 <muditsharma1087@gmail.com>
2024-10-22 18:20:37 +0530

    Add files via upload

---
+++ day 20 chefland games easy .c .c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int T; 
+    scanf("%d", &T);
+    
+    for (int i = 0; i < T; i++) {
+        int R1, R2, R3, R4; 
+        scanf("%d %d %d %d", &R1, &R2, &R3, &R4);
+        
+        if (R1 == 0 && R2 == 0 && R3 == 0 && R4 == 0) {
+            printf("IN\n"); 
+        } else {
+            printf("OUT\n"); 
+        }
+    }
+    
+    return 0;
+}
