c24b1b1133af079301c369de65032c08f78414fa
Muditsharma0001 <muditsharma1087@gmail.com>
2024-11-05 23:42:27 +0530

    Add files via upload

---
+++ day 34 easy.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int T;
+    scanf("%d", &T);
+    while (T--) {
+        int A, B; 
+        scanf("%d %d", &A, &B);
+
+        int C = 21 - A - B;
+
+          if (C >= 1 && C <= 10) {
+            printf("%d\n", C);
+        } else {
+            printf("-1\n"); 
+        }
+    }
+
+    return 0;
+}
